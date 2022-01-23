1155468a17e617db5797493fb6bde1b48d9b5b9d
hossein <hossein13mousavi@gmail.com>
2022-01-23 16:11:20 +0330

    feat(global)!: implement project layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'rmd-front';
+export class AppComponent implements OnInit {
+  public isDarkTheme: boolean = true;
+  public screenWidth: number = window.innerWidth;
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  @ViewChild('infoSidenav') infoSidenav!: MatSidenav;
+  @HostListener('window:resize', ['$event'])
+  public onResize() {
+    this.screenWidth = window.innerWidth;
+  }
+
+  public clickHandler(): void {
+    this.sidenav.close().finally();
+  }
+
+  ngOnInit(): void {
+    this.isDarkTheme = localStorage.getItem('theme') === 'dark';
+  }
+
+  public changeTheme(): void {
+    this.isDarkTheme = !this.isDarkTheme;
+    localStorage.setItem('theme', this.isDarkTheme ? 'dark' : 'light');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
