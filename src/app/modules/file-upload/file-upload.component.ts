113d4690156ba94690a3ad0f72c4f0dc5b9b1841
hossein <hossein13mousavi@gmail.com>
2022-01-31 15:05:13 +0330

    feat(appModule): implement logic of uploading a file

--- src/app/modules/file-upload/file-upload.component.ts
+++ src/app/modules/file-upload/file-upload.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../../services/app.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Organization, Report, ReportInfo } from '../../models/common.model';
 
 @Component({
   selector: 'app-file-upload',
@@ -6,7 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./file-upload.component.scss'],
 })
 export class FileUploadComponent implements OnInit {
-  constructor() {}
+  public organizations: Array<Organization> = [];
+  public reportType: Array<Report> = [];
+  public form: FormGroup = this.fb.group({
+    organization: [null, [Validators.required]],
+    reportType: [null, [Validators.required]],
+    reportTypeId: [null, [Validators.required]],
+    type: [1, [Validators.required]],
+    date: [Date.now(), [Validators.required]],
+    file: [null, [Validators.required]],
+  });
 
-  ngOnInit(): void {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.getOrganizationList();
+    this.form
+      .get('organization')
+      ?.valueChanges.subscribe((value) => this.getReportTypes(value));
+  }
+
+  private getOrganizationList(): void {
+    this.appService
+      .getOrganization()
+      .subscribe((response) => (this.organizations = response));
+  }
+
+  private getReportTypes(organizationCode: string): void {
+    this.appService
+      .getReportTypes(organizationCode)
+      .subscribe((response) => (this.reportType = response));
+  }
+
+  public uploadFile(): void {
+    const reportInfo = this.form.value as ReportInfo;
+    this.appService
+      .uploadReport(reportInfo)
+      .subscribe(() => console.log('success'));
+  }
 }
