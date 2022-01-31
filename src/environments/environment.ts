4633472fc91736f601e23c9d4ffed6524007e693
hossein <hossein13mousavi@gmail.com>
2022-01-31 14:49:00 +0330

    feat(global)!: add api url (temp url danial's computer)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  rmdAPI: 'http://172.22.255.239:3000',
 };
 
 /*
