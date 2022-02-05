9d11e8d51db2dbe448b98fc2ed2cf927979eb537
hossein <hossein13mousavi@gmail.com>
2022-02-05 16:58:51 +0330

    fix(fileUpload): fix problem with form

--- src/app/modules/file-upload/file-upload.module.ts
+++ src/app/modules/file-upload/file-upload.module.ts
@@ -1,9 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileUploadComponent } from './file-upload.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [FileUploadComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatTooltipModule,
+  ],
+  exports: [FileUploadComponent],
 })
 export class FileUploadModule {}
