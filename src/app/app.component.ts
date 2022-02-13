5ea75cee76c9facdc6898f60d9c72e639dd9f740
hossein <hossein13mousavi@gmail.com>
2022-02-13 11:57:07 +0330

    feat(routing): implement navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { AfterContentInit, Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { Location } from '@angular/common';
-import { ValidRoutes } from './models/common.model';
+import { ValidRoutes, ValidRouteTranslation } from './models/common.model';
 import { ValidRouteTranslator } from './const/constants';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -11,6 +12,7 @@ import { ValidRouteTranslator } from './const/constants';
 })
 export class AppComponent implements OnInit, AfterContentInit {
     public matCardHeader!: string;
+    public navigationList: Array<ValidRouteTranslation> = ValidRouteTranslator;
     public isDarkTheme: boolean = true;
     public screenWidth: number = window.innerWidth;
     @ViewChild('sidenav') sidenav!: MatSidenav;
@@ -20,16 +22,17 @@ export class AppComponent implements OnInit, AfterContentInit {
         this.screenWidth = window.innerWidth;
     }
 
-    constructor(private readonly location: Location) {}
+    constructor(private readonly location: Location, private readonly router: Router) {}
 
-    public clickHandler(): void {
-        this.sidenav.close().finally();
+    public clickHandler(navigationItem: ValidRouteTranslation): void {
+        this.router.navigate([navigationItem.englishTitle]).finally(() => {
+            this.sidenav.close().finally(() => this.setHeaderBasedOnCurrentRoute());
+        });
     }
 
     private setHeaderBasedOnCurrentRoute(): void {
         const path: ValidRoutes = this.location.path().slice(1) as ValidRoutes;
         // the slice is for removing the / from the beginning of the path
-
         this.matCardHeader = ValidRouteTranslator.find((validRoute) => validRoute.englishTitle === path)!.persianTitle;
     }
 
--- src/app/const/constants.ts
+++ src/app/const/constants.ts
@@ -1,7 +1,7 @@
 import { ValidRouteTranslation } from '../models/common.model';
 
 export const ValidRouteTranslator: Array<ValidRouteTranslation> = [
+    { persianTitle: 'خانه', englishTitle: '' },
     { persianTitle: 'بارگزاری فایل', englishTitle: 'upload-file' },
     { persianTitle: 'داشبورد', englishTitle: 'dashboard' },
-    { persianTitle: 'خانه', englishTitle: '' },
 ];
